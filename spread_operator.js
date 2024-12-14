db26da6746b035ce935e5b34cb7a9e3be5531a6c
Fabio <payoartudi@hotmail.it>
2024-12-14 17:45:59 +0100

    aggiunti esercizi rest parameter e spread operator

---
+++ spread_operator.js
@@ -0,0 +1,25 @@
+// esercizio 1
+
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+
+const mergedArray = [...arr1, ...arr2];
+
+console.log(mergedArray);
+
+// esercizio 2
+
+const obj1 = { a: 1, b: 2 };
+const obj2 = { b: 3, c: 4 };
+
+const mergedObject = { ...obj1, ...obj2 };
+
+console.log(mergedObject);
+
+// esercizio 3
+
+const originalArray = [1, 2, 3, 4, 5];
+
+const cloneArray = [...originalArray];
+
+console.log(cloneArray);
